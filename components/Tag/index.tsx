d79df78567486584369e4034f597176188730514
Tsuzien <personal_rogers@outlook.com>
2023-01-21 11:31:18 +0800

    docs: css选择器优先级问题

--- components/Tag/index.tsx
+++ components/Tag/index.tsx
@@ -6,7 +6,7 @@ import { TAGS } from '../../config/slug-config';
 const Tag = ({ className }: { className: string }) => {
   return (
     <div className={classNames(className, 'mx-4 px-6 sticky top-20')}>
-      <div className="text-lg text-purple-400 font-bold">热门标签</div>
+      <div className="text-lg text-purple-400 font-bold">标签</div>
       <div className="mt-6">
         {TAGS.map((tag) => (
           <span key={tag.label} className="inline-block m-2">
