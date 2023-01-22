063611246f3124ceadd6288868e9b64229ae9e99
Tsuzien <personal_rogers@outlook.com>
2023-01-22 11:52:11 +0800

    perf: 404 mobile page

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,8 +6,8 @@ export default function Custom404() {
     <>
       <div className="bg-black w-screen h-screen">
         <div className="mx-auto w-fit top-1/3 relative text-gray-50">
-          <div className="text-9xl ">Not Found</div>
-          <div className="text-6xl ">There is nothing.</div>
+          <div className="text-6xl md:text-9xl">Not Found</div>
+          <div className="text-4xl md:text-6xl">There is nothing.</div>
           <div className="py-10 text-2xl text-yellow-400">
             <BsArrowUpLeft />
             <Link href="/" className="text-yellow-400 pl-4">
