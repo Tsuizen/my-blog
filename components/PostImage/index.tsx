a5963e9ea9883c6260ad1bf7ee60b21b5db22335
Tsuzien <personal_rogers@outlook.com>
2023-03-07 12:54:11 +0800

    feat: 🎸 img center & new-bing

--- components/PostImage/index.tsx
+++ components/PostImage/index.tsx
@@ -2,21 +2,23 @@ import Image from 'next/image';
 
 const PostImage = ({ alt, src }) => {
   return (
-    <Image
-      className="w-full my-6"
-      alt={alt}
-      src={src}
-      width={700}
-      height={475}
-      // sizes="100vw"
-      // style={{
-      //   width: '100%',
-      //   height: 'auto'
-      // }}
-      quality={100}
-      placeholder="blur"
-      blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
-    ></Image>
+    <span className=" text-center block">
+      <Image
+        className="my-6 mx-auto"
+        alt={alt}
+        src={src}
+        width={600}
+        height={400}
+        // sizes="100vw"
+        // style={{
+        //   width: '100%',
+        //   height: 'auto'
+        // }}
+        quality={75}
+        placeholder="blur"
+        blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+      ></Image>
+    </span>
   );
 };
 
